5d7f7f1ab4d939aeeb2571f70f5d7f486ef7a57c
andykov <abb.name@gmail.com>
2018-11-27 16:50:20 +0300

    fixed close modal stock

--- src/scripts/template.js
+++ src/scripts/template.js
@@ -25,13 +25,46 @@ $(document).ready(function () {
     $('.services__inner').height(maxHeight);
 
     // Показ акции при попытке закрыть страницу
-    $(document).mouseleave(function(e){
-        if (e.clientY < 10) {
-            $.fancybox.open( $('#stock'), {
-                touch: false
-            });
-        }
-    });
+    function getCookie(name) {
+        var matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+
+    // проверка, если cookie установлена - не показываем, если cookie не установлена - показываем
+    var displayStock = getCookie("display_stock");
+    if (displayStock != "no") {
+        $(document).mouseleave(function(e){
+
+            if (e.clientY < 0) {
+
+                if ($('#stock').length) {
+                    $.fancybox.open({
+                        src  : '#stock',
+                        type : 'inline',
+                        opts : {
+                            touch: false,
+                            afterShow: function( instance, current ) {
+                                console.log( 'done!' );
+                            },
+                            afterClose: function( instance, current ) {
+                                console.log( 'done CLOSW!' );
+                                console.log($(current.src).remove());
+                            }
+                        }
+                    });
+                }
+
+                // записываем cookie на 1 день, с которой мы не показываем окно
+                var date = new Date;
+                date.setDate(date.getDate() + 1);
+                document.cookie = "display_stock=no; path=/; expires=" + date.toUTCString();
+            }
+        });
+    }
+
 
     // menu
     var sections = $('.section');
@@ -74,7 +107,7 @@ $(document).ready(function () {
                 var h = $(this).attr('data-height');
                 $(this).height($(window).width() >= 960 ? h : (h/1.4)).find('span').css('opacity', 1);
             });
-            
+
             this.destroy();
         },
         offset: '50%'
@@ -111,14 +144,14 @@ $(document).ready(function () {
 
     function cilinder() {
         $('.cilinder-lines svg').attr('viewBox', "0 0 " + $('.cilinder-lines').width() + " " + $('.cilinder-lines').height());
-        
+
         $('.steps__item').each(function (i, e) {
             var cylinder = $(this).find('.steps__step');
             var spanTop = cylinder.position().top;
             var spanLeft = cylinder.position().left;
             var spanWidth = cylinder.width();
             var spanHeight = cylinder.height();
-        
+
 
             if ($(this).next().length) {
                 console.log('NEXT');
@@ -127,7 +160,7 @@ $(document).ready(function () {
                 var spanLeftNext = cylinderNext.position().left;
                 var spanWidthNext = cylinderNext.width();
                 var spanHeightNext = cylinderNext.height();
-        
+
                 $('.cilinder-lines svg line').eq(i).attr({
                     'x1': spanLeft + (spanWidth / 2),
                     'y1': spanTop + (spanHeight / 2),
@@ -227,7 +260,7 @@ $(document).ready(function () {
         offset: '60%'
     });
 
-    
+
     // services dropdown
     $('.services__inner').on('click', function(){
         $(this).parent().toggleClass('is-open');
@@ -244,40 +277,40 @@ $(document).ready(function () {
 
     // Create Countdown
     var Countdown = {
-    
+
         // Backbone-like structure
         $el: $('.countdown'),
-        
+
         // Params
         countdown_interval: null,
         total_seconds     : 0,
-        
-        // Initialize the countdown  
+
+        // Initialize the countdown
         init: function() {
-        
+
         // DOM
             this.$ = {
             hours  : this.$el.find('.bloc-time.hours .figure'),
             minutes: this.$el.find('.bloc-time.min .figure'),
             seconds: this.$el.find('.bloc-time.sec .figure')
             };
-    
+
         // Init countdown values
         this.values = {
                 hours  : this.$.hours.parent().attr('data-init-value'),
             minutes: this.$.minutes.parent().attr('data-init-value'),
             seconds: this.$.seconds.parent().attr('data-init-value'),
         };
-        
+
         // Initialize total seconds
         this.total_seconds = this.values.hours * 60 * 60 + (this.values.minutes * 60) + this.values.seconds;
-    
-        // Animate countdown to the end 
-        this.count();    
+
+        // Animate countdown to the end
+        this.count();
         },
-        
+
         count: function() {
-        
+
         var that    = this,
             $hour_1 = this.$.hours.eq(0),
             $hour_2 = this.$.hours.eq(1),
@@ -285,105 +318,105 @@ $(document).ready(function () {
             $min_2  = this.$.minutes.eq(1),
             $sec_1  = this.$.seconds.eq(0),
             $sec_2  = this.$.seconds.eq(1);
-        
+
             this.countdown_interval = setInterval(function() {
-    
+
             if(that.total_seconds > 0) {
-    
-                --that.values.seconds;              
-    
+
+                --that.values.seconds;
+
                 if(that.values.minutes >= 0 && that.values.seconds < 0) {
-    
+
                     that.values.seconds = 59;
                     --that.values.minutes;
                 }
-    
+
                 if(that.values.hours >= 0 && that.values.minutes < 0) {
-    
+
                     that.values.minutes = 59;
                     --that.values.hours;
                 }
-    
+
                 // Update DOM values
                 // Hours
                 that.checkHour(that.values.hours, $hour_1, $hour_2);
-    
+
                 // Minutes
                 that.checkHour(that.values.minutes, $min_1, $min_2);
-    
+
                 // Seconds
                 that.checkHour(that.values.seconds, $sec_1, $sec_2);
-    
+
                 --that.total_seconds;
             }
             else {
                 clearInterval(that.countdown_interval);
             }
-        }, 1000);    
+        }, 1000);
         },
-        
+
         animateFigure: function($el, value) {
-        
+
         var that         = this,
                 $top         = $el.find('.top'),
             $bottom      = $el.find('.bottom'),
             $back_top    = $el.find('.top-back'),
             $back_bottom = $el.find('.bottom-back');
-    
+
         // Before we begin, change the back value
         $back_top.find('span').html(value);
-    
+
         // Also change the back bottom value
         $back_bottom.find('span').html(value);
-    
+
         // Then animate
         TweenMax.to($top, 0.8, {
             rotationX           : '-180deg',
             transformPerspective: 300,
                 ease                : Quart.easeOut,
             onComplete          : function() {
-    
+
                 $top.html(value);
-    
+
                 $bottom.html(value);
-    
+
                 TweenMax.set($top, { rotationX: 0 });
             }
         });
-    
-        TweenMax.to($back_top, 0.8, { 
+
+        TweenMax.to($back_top, 0.8, {
             rotationX           : 0,
             transformPerspective: 300,
-                ease                : Quart.easeOut, 
-            clearProps          : 'all' 
-        });    
+                ease                : Quart.easeOut,
+            clearProps          : 'all'
+        });
         },
-        
+
         checkHour: function(value, $el_1, $el_2) {
-        
+
         var val_1       = value.toString().charAt(0),
             val_2       = value.toString().charAt(1),
             fig_1_value = $el_1.find('.top').html(),
             fig_2_value = $el_2.find('.top').html();
-    
+
         if(value >= 10) {
-    
+
             // Animate only if the figure has changed
             if(fig_1_value !== val_1) this.animateFigure($el_1, val_1);
             if(fig_2_value !== val_2) this.animateFigure($el_2, val_2);
         }
         else {
-    
+
             // If we are under 10, replace first figure with 0
             if(fig_1_value !== '0') this.animateFigure($el_1, 0);
             if(fig_2_value !== val_1) this.animateFigure($el_2, val_1);
-        }    
+        }
         }
     };
 
     // init timer
-    Countdown.init(); 
-  
+    Countdown.init();
+
 });
 
 
